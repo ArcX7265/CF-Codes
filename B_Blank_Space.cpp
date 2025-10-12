5499dda268f8b79c3251d19b30f545f759dd0b9f
Adarsh Raj Choudhary <arcx7265@gmail.com>
2025-10-12 12:06:01 +0530

    Added a new file

---
+++ B_Blank_Space.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;cin>>t;
+while(t--){
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+int count=0;
+int maxi=0;
+for(int i=0;i<n;i++){
+    if(arr[i]==0){
+        count++;
+        maxi=max(count,maxi);
+    }
+    else{
+        count=0;
+    }
+}
+cout<<maxi<<"\n";
+}
+return 0;
+}
