805e117644a0a0e994addb739602cf91d8d3c3c0
Adarsh Raj Choudhary <arcx7265@gmail.com>
2025-11-01 11:11:34 +0530

    Added a new file

---
+++ A_Sum_of_Odd_Integers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;cin>>t;
+while(t--){
+long long n,k;
+cin>>n>>k;
+if(n>=k*k){
+    if(n%2==0 && k%2==0){
+        cout<<"YES"<<endl;
+    }
+    else if(n%2!=0 && k%2!=0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+else{
+    cout<<"NO"<<endl;
+}
+}
+return 0;
+}
