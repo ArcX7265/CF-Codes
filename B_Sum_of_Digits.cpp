a949840d9c99bae0fa0e9a90faece81a41f29178
Adarsh Raj Choudhary <arcx7265@gmail.com>
2025-11-18 12:32:17 +0530

    Added a new file

---
+++ B_Sum_of_Digits.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+
+    if (n.size() == 1) {
+        cout << 0;
+        return 0;
+    }
+
+    int steps = 0;
+
+    while (n.size() > 1) {
+        long long sum = 0;
+        for (char c : n)
+            sum += (c - '0');
+
+        n = to_string(sum);
+        steps++;
+    }
+
+    cout << steps;
+    return 0;
+}
